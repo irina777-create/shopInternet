e527332aa48b6bb2a011b5b1f35b7df767dbdff1
irina777-create <71390012+irina777-create@users.noreply.github.com>
2023-12-07 22:12:21 +0200

    изменила index.html

--- main.js
+++ main.js
@@ -5,197 +5,132 @@
 // При натисканні на “купити” з'являється повідомлення, що товар куплений і повернення у вихідний стан програми ( коли відображається лише список категорій).
 
 
-const category =document.querySelector('li')
-categorу.addEventListener('click',chooseCateg)
-function chooseCateg(event){
+const category =document.querySelector('ul')
+categorу.addEventListener('click',chooseShop)
+function chooseShop(event){
     const choosenCatigory = Array.from(category.children)
-    switch (event. target. textContent){
-        case choosenCatigory[0].textContent:
-            ChooseShop.showCats;
+    switch (event.target.textContent){
+        case  choosenCatigory[0].textContent:
+            ShowGoods.showCats();
             break;
-        case choosenCatigory[1].textContent:
-            ChooseShop.showDogs;
+        case  choosenCatigory[1].textContent:
+            ShowGoods.showDogs()
             break;
-            case choosenCatigory[2].textContent:
-                ChooseShop.showBirds;
-                break;
-        case choosenCatigory[3].textContent:
-            ChooseShop.showRodents;
+        case  choosenCatigory[2].textContent:
+            ShowGoods.showBirds()
+            break;
+        case  choosenCatigory[3].textContent:
+            ShowGoods.showRodents()
             break;
 
     }
 }
-function chooseCats(event) {
-    const info = document.querySelector('.info');
-    switch (event.target.textContent) {
-        case 'Feed':
-            createInfo(shopInfo.cats.feed)
-            break;
-        case 'Health':
-            createInfo(shopInfo.cats.health)
-            break;
-        case 'Accesories':
-            createInfo(shopInfo.cats.accessories)
-            break;
-        case 'Wordrobe':
-            createInfo(shopInfo.cats.wordrobe)
-            break;
+class ShowShop{
+    static shop = document.querySelector('.shop')
+    static showCats(){
+        this.shop.innerHTML ='<h2>Товари</h2><ul><li>Feed</li><li>Health</li><li>Accesories</li><li>Wordrobe</li></ul>'
+        this.shop.addEventListener('click', chooseCats)
+    }
+    static showDogs(){
+        this.shop.innerHTML ='<h2>Товари</h2><ul><li>Feed</li><li>Health</li>Accesories<li>Wordrobe</li></ul>'
+        this.goods.addEventListener('click', chooseDogs)
+    }
+    static showBirds(){
+        this.shop.innerHTML ='<h2>Товари</h2><ul><li>Feed</li><li>Health</li><li>Accesories</li><li>Wordrobe</li></ul>'
+        this.shop.addEventListener('click', chooseBirds())
+    }
+    static showRodents(){
+        this.shop.innerHTML ='<h2>Товари</h2><ul><li>Feed</li><li>Health</li><li>Accesories</li><li>Wordrobe</li></ul>'
+        this.shop.addEventListener('click', chooseRodents())
     }
 }
+
+function chooseCats(event){
+    const info = document.querySelector('.info');
+    switch(event.target.textContent){
+        case 'Feed':
+            info.innerHTML = '<h2>Інформация</h2><h3>Royal Canin Kitten</h3><img src="https://petchoice.ua/koshkam/suxoj-korm/royal-canin-kittent-dlya-kotyat"><button>Купить</button>;'
+            break;
+        case 'Health':
+            info.innerHTML = '<h2>Інформация</h2><h3>Vitamin</h3><img src="https://petchoice.ua/koshkam/vitaminyi-i-dobavki"><button>Купить</button>;'
+            break;
+        case 'Accesories':
+            info.innerHTML = '<h2>Інформация</h2><h3>Ammunition</h3><img src="https://petchoice.ua/koshkam/povodki-shlei"><button>Купить</button>;'
+            break;
+        case 'Wordrobe':
+            info.innerHTML = '<h2>Інформация</h2><h3>Set of clothes</h3><img src="https://petchoice.ua/koshkam/odezhda"><button>Купить</button>;'
+            break;
+
+    }
     buttonOn()
-
-function chooseDogs(event) {
+}
+function chooseDogs(event){
     const info = document.querySelector('.info');
-    switch (event.target.textContent) {
+    switch(event.target.textContent){
         case 'Feed':
-            createInfo(shopInfo.dogs.feed)
+            info.innerHTML = '<h2>Інформация</h2><h3>Royal Canin </h3><img src="https://petchoice.ua/sobakam/suxoj-korm/royal-canin-maxi-adult-dlya-sobak-krupnyix-porod"><button>Купить</button>;'
             break;
         case 'Health':
-            createInfo(shopInfo.dogs.health)
+            info.innerHTML = '<h2>Інформация</h2><h3>Vitamin</h3><img src="https://petchoice.ua/sobakam/vitaminyi-i-dobavki"><button>Купить</button>;'
             break;
         case 'Accesories':
-            createInfo(shopInfo.dogs.accessories)
+            info.innerHTML = '<h2>Інформация</h2><h3>Ammunition</h3><img src="https://petchoice.ua/sobakam/povodki-ruletki-shlei"><button>Купить</button>;'
             break;
         case 'Wordrobe':
-            createInfo(shopInfo.dogs.wordrobe)
+            info.innerHTML = '<h2>Інформация</h2><h3>Set of clothes</h3><img src="https://petchoice.ua/sobakam/odezhda"><button>Купить</button>;'
             break;
-    }
-}
-buttonOn()
 
-function chooseBirds(event) {
+    }
+    buttonOn()
+}
+
+function chooseBirds(event){
     const info = document.querySelector('.info');
-    switch (event.target.textContent) {
+    switch(event.target.textContent){
         case 'Feed':
-            createInfo(shopInfo.birds.feed)
+            info.innerHTML = '<h2>Інформация</h2><h3>Versele-Laga Prestig </h3><img src="https://petchoice.ua/pticzam/korm/versele-laga-prestige-korm-dlya-volnistyix-popugajchikov"><button>Купить</button>;'
             break;
         case 'Health':
-            createInfo(shopInfo.birds.health)
+            info.innerHTML = '<h2>Інформация</h2><h3>Vitamin</h3><img src="https://petchoice.ua/pticzam/vitaminyi-i-dobavki"><button>Купить</button>;'
             break;
         case 'Accesories':
-            createInfo(shopInfo.birds.accessories)
+            info.innerHTML = '<h2>Інформация</h2><h3>Ammunition</h3><img src="https://petchoice.ua/pticzam/aksessuaryi-dlya-kletok"><button>Купить</button>;'
             break;
         case 'Wordrobe':
-            createInfo(shopInfo.birds.wordrobe)
+            info.innerHTML = '<h2>Інформация</h2><h3>Cells and Varieties</h3><img src="https://petchoice.ua/pticzam/kletki-i-perenoski"><button>Купить</button>;'
             break;
+
     }
+    buttonOn()
 }
-buttonOn()
 
 function chooseRodents(event) {
     const info = document.querySelector('.info');
     switch (event.target.textContent) {
         case 'Feed':
-            createInfo(shopInfo.rodentss.feed)
+            info.innerHTML = '<h2>Інформация</h2><h3>Padovan Fieno-Hay</h3><img src="https://petchoice.ua/gryizunam/padovan-fieno-hay"><button>Купить</button>;'
             break;
         case 'Health':
-            createInfo(shopInfo.rodents.health)
+            info.innerHTML = '<h2>Інформация</h2><h3>Vitamin</h3><img src="https://petchoice.ua/gryizunam/vitaminyi-i-dobavki"><button>Купить</button>;'
             break;
         case 'Accesories':
-            createInfo(shopInfo.rodents.accessories)
+            info.innerHTML = '<h2>Інформация</h2><h3>Ammunition</h3><img src="https://petchoice.ua/gryizunam/kormushki"><button>Купить</button>;'
             break;
         case 'Wordrobe':
-            createInfo(shopInfo.rodents.wordrobe)
+            info.innerHTML = '<h2>Інформация</h2><h3>Set of clothes</h3><img src="https://petchoice.ua/gryizunam/domiki-i-lezhanki"><button>Купить</button>;'
             break;
+
     }
+    buttonOn()
 }
-buttonOn()
-
-
-const shopInfo{
-
-    cats:{
-        Feed:{
-            name:'Royal Canin Kitten'
-            picture:'https://petchoice.ua/koshkam/suxoj-korm/royal-canin-kittent-dlya-kotyat'
-
-        }
-        Health:{
-            name:'Vitamin'
-            picture:'https://petchoice.ua/koshkam/vitaminyi-i-dobavki'
-
-        }
-        Accessories:{
-            name:'Ammunition'
-            picture:'https://petchoice.ua/koshkam/povodki-shlei'
-
-        }
-        Wordrobe:{
-            name:'Set of clothes'
-            picture:'https://petchoice.ua/koshkam/odezhda'
-        }
-
-    },
-,
-    dogs:{
-        Feed:{
-            name:'Royal Canin'
-            picture:'https://petchoice.ua/sobakam/suxoj-korm/royal-canin-maxi-adult-dlya-sobak-krupnyix-porod'
-
-        }
-        Health:{
-            name:'Vitamin'
-            picture:'https://petchoice.ua/sobakam/vitaminyi-i-dobavki'
-
-        }
-        Accessories:{
-            name:'Ammunition'
-            picture:'https://petchoice.ua/sobakam/povodki-ruletki-shlei'
-
-        }
-        Wordrobe:{
-            name:'Set of clothes'
-            picture:'https://petchoice.ua/sobakam/odezhda'
-        }
-
-    },
-
-    birds:{
-        Feed:{
-            name:'Versele-Laga Prestige'
-            picture:'https://petchoice.ua/pticzam/korm/versele-laga-prestige-korm-dlya-volnistyix-popugajchikov'
-
-        }
-        Health:{
-            name:'Vitamin'
-            picture:'https://petchoice.ua/pticzam/vitaminyi-i-dobavki'
-
-        }
-        Accessories:{
-            name:'Ammunition'
-            picture:'https://petchoice.ua/pticzam/aksessuaryi-dlya-kletok'
-
-        }
-        Wordrobe:{
-            name:'Cells and Varieties'
-            picture:'https://petchoice.ua/pticzam/kletki-i-perenoski'
-        }
-
-    },
-    rodents:{
-        Feed:{
-            name:'Padovan Fieno-Hay '
-            picture:'https://petchoice.ua/gryizunam/padovan-fieno-hay'
-
-        }
-        Health:{
-            name:'Vitamin'
-            picture:'https://petchoice.ua/gryizunam/vitaminyi-i-dobavki'
-
-        }
-        Accessories:{
-            name:'Ammunition'
-            picture:'https://petchoice.ua/gryizunam/kormushki'
-
-        }
-        Wordrobe:{
-            name:'Set of clothes'
-            picture:'https://petchoice.ua/gryizunam/domiki-i-lezhanki'
-        }
+function buttonOn() {
+    const button = document.querySelector('button')
+    button.addEventListener('click', onchase)
+    function onchase(){
+        const name = document.querySelector('h3')
+        alert(`Ваша покупка ${name.textContent}`)
+        location.reload()
     }
 }
 
-    function createInfo(item){
-        const info = document.querySelector('.info');
-        info.innerHTML =`<h2>Інформация</h2><h3>${item.name}</h3><img src=${item.picture}><button>Купити</button>;`;
-        buttonOn()
+
